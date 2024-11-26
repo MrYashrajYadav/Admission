ec80141a76abb700ad2c5dcd82187fc552189ef5
Yashraj Yadav <104068490+MrYashrajYadav@users.noreply.github.com>
2024-11-26 20:54:01 +0530

    Update main_script.js

--- javascript/main_script.js
+++ javascript/main_script.js
@@ -1,3 +1,11 @@
+// Wait for the page to load
+window.addEventListener('load', function() {
+    // Hide the loading animation and show the content after 2 seconds
+    setTimeout(function() {
+        document.getElementById('loading').style.display = 'none';
+    }, 2000); // You can adjust the timeout for the loading screen duration
+});
+
 var navLinks= document.getElementById("navLinks");
 
 function showmenu(){
